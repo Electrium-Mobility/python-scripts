aef7900571acc59dd305cc0dca8626ae3c5c2ca1
Tom Wang <tomwangzz1236@gmail.com>
2024-11-27 01:08:14 -0500

    11/27 updates

--- src/predefiend.py
+++ src/predefiend.py
@@ -9,16 +9,16 @@ This script contains predefined values that are subject to change
 '''
 
 #1. List of Projects
-projects = ['Electric Bike (8-month)', 'Electric Scooter', 'Electric Skateboard', 'Balance Board', 'Electric Motorcycle',
-            'Electric Go-Kart', 'Electric Couch', 'Custom Electronics', 'Other']
+projects = ['Electric Longtail Conversion Kit', 'Onewheel', 'Electric Road Bike', 'Gokart', 'Electric Sofa',
+            'Custom ESC', 'Custom Remote Control', 'Battery Management System', 'Other']
 
 
 #2. Max people for each project
-capacities={'Electric Bike (8-month)': 130,'Electric Scooter':130,'Electric Skateboard':310, 'Balance Board': 130,
-            'Electric Motorcycle':130, 'Electric Go-Kart': 130, 'Electric Couch': 130, 'Custom Electronics':130, 
-            'Other': 130 } 
+capacities={'Electric Longtail Conversion Kit': 10,'Onewheel':10,'Electric Road Bike':10, 'Gokart': 10,
+            'Electric Sofa':10, 'Custom ESC': 10, 'Custom Remote Control': 10, 'Battery Management System':10, 
+            'Other': 10 } 
 
 #3. Min people for each project
-demands={'Electric Bike (8-month)': 0,'Electric Scooter':0,'Electric Skateboard':0, 'Balance Board': 0,
-            'Electric Motorcycle':0, 'Electric Go-Kart': 0, 'Electric Couch': 0, 'Custom Electronics':0, 
-            'Other': 0 } 
+demands={'Electric Longtail Conversion Kit': 4,'Onewheel': 4,'Electric Road Bike':4, 'Gokart': 4,
+            'Electric Sofa': 4, 'Custom ESC': 4, 'Custom Remote Control': 0, 'Battery Management System':4, 
+            'Other': 4} 
